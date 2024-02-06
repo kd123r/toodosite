fe80b2e071a88966107338c4ee45f5d3e6a7e8b3
Kevin Hoang Dang <kdang65@gatech.edu>
2024-02-06 17:39:49 -0500

    added upvote, follower, notification models

--- toodo/admin.py
+++ toodo/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import PrivateTodo, PublicTodo
+from .models import PrivateTodo, PublicTodo, Upvote, Follower, Notification
 # Register your models here.
 
 admin.site.register(PrivateTodo)
 admin.site.register(PublicTodo)
+admin.site.register(Upvote)
+admin.site.register(Follower)
+admin.site.register(Notification)
--- toodo/models.py
+++ toodo/models.py
@@ -18,3 +18,42 @@ class PublicTodo(models.Model):
     todo_text = models.CharField(max_length=200)
     upvotes = models.IntegerField(default=0)
     pub_date = models.DateTimeField()
+
+class Upvote(models.Model):
+    public_todo = models.ForeignKey(
+        PublicTodo, 
+        on_delete=models.CASCADE
+        )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+class Follower(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+    following = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followers'
+    )
+
+class Notification(models.Model):
+    class Action(models.IntegerChoices):
+        UPVOTE = 1
+        FOLLOW = 2
+    action = models.IntegerField(choices=Action.choices)
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='sent_notifications'
+    )
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='received_notifications'
+    )
+    timestamp = models.DateTimeField()
